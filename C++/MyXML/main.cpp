cebab942a961a1651c96e3d07a628d699f163ae4
xiaozhengxing <1084287435@qq.com>
2019-04-15 21:10:23 +0800

    Update main.cpp

--- C++/MyXML/main.cpp
+++ C++/MyXML/main.cpp
@@ -1,1 +1,14 @@
-todo
+#include "XML.h"
+
+class XmlFile;
+
+int main()
+{
+	XmlFile xmlFile;
+	xmlFile.readFile("test.xml");
+	xmlFile.eraseComment();
+	xmlFile.buildXmlTree();
+
+	xmlFile.printTree();
+	system("pause");
+}
